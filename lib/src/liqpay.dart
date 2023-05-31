579bcb2a23c2c730d107b77412e9373b90cb1b1c
dimashimko <dimashimko7@gmail.com>
2023-05-31 17:06:41 +0300

    Update liqpay.dart

--- lib/src/liqpay.dart
+++ lib/src/liqpay.dart
@@ -38,11 +38,6 @@ class LiqPay {
     }
   }
 
-  String _createSignature(String data) {
-    final signatureString = privateKey + data + privateKey;
-    return base64.encode(sha1.convert(utf8.encode(signatureString)).bytes);
-  }
-
   /// Performs checkout request to LiqPay with [order]
   /// This will actually return redirect url location.
   Future<String> checkout(LiqPayOrder order) async {
@@ -60,19 +55,76 @@ class LiqPay {
     }
   }
 
+  /// Performs checkout request to LiqPay with [order]
+  /// This will actually return redirect url location.
+  Future<String> status(String id) async {
+    Map<String, String> params = {};
+    params['action'] = 'status';
+    params['order_id'] = id;
+
+    final url = Uri.https(kHost, kServerApiEndpoint);
+
+    final http.Response response =
+        await client.post(url, body: getRequestDataFromMap(params));
+
+    if (response.statusCode == 200) {
+      // log('response.statusCode == 200');
+      Map responseMap = json.decode(response.body);
+      final String? status = responseMap["status"];
+      if (status == 'success') {
+        return 'success';
+      } else if (status == 'error') {
+        final String? errCode = responseMap["err_code"];
+        if (errCode == null) return 'Something wrong';
+        return errCode;
+      }
+      return status ?? '';
+
+      // print('status: $status');
+
+      // final categories = List<Map<String, dynamic>>.from(response.data['data']);
+
+/*      final location = response.headers["location"] ??
+          (throw HttpException(response.toString(), uri: url));*/
+      // return response.body;
+    } else if (response.statusCode == 302) {
+      // log('response.statusCode == 302');
+      final String location = response.headers["location"] ??
+          (throw HttpException(response.toString(), uri: url));
+      // log("Redirect location: $location");
+      return location;
+    } else {
+      // log('response.statusCode == ???');
+      // log('response.toString() : ${response.toString()}');
+      throw HttpException(response.toString(), uri: url);
+    }
+    return '';
+  }
+
   /// Returns request data and signature from the [order] object.
   /// Resulting map will contain values within "data" and "signature" keys.
-  Map<String, String> getRequestData(LiqPayOrder order) {
-    final jsonObject = order.toJson();
+  Map<String, String> getRequestDataFromMap(Map<String, dynamic> jsonObject) {
     jsonObject['public_key'] = publicKey;
     jsonObject['version'] = kApiVersion.toString();
 
     final jsonString = json.encode(jsonObject);
-    log("Request json: $jsonString");
+    // log("Request json: $jsonString");
 
     final data = base64.encode(utf8.encode(jsonString));
     final signature = _createSignature(data);
 
     return {"data": data, "signature": signature};
+  }
+
+  /// Returns request data and signature from the [order] object.
+  /// Resulting map will contain values within "data" and "signature" keys.
+  Map<String, String> getRequestData(LiqPayOrder order) {
+    final Map<String, dynamic> jsonObject = order.toJson();
+    return getRequestDataFromMap(jsonObject);
+  }
+
+  String _createSignature(String data) {
+    final signatureString = privateKey + data + privateKey;
+    return base64.encode(sha1.convert(utf8.encode(signatureString)).bytes);
   }
 }
